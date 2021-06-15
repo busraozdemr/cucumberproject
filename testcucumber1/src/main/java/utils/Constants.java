4daeb7020e7c9b5800b933b13348cd738b9b5ae6
busraozdemr <48418589+busraozdemr@users.noreply.github.com>
2021-06-15 23:59:44 +0300

    constant minor fix

--- testcucumber1/src/main/java/utils/Constants.java
+++ testcucumber1/src/main/java/utils/Constants.java
@@ -1,7 +1,5 @@
 package utils;
 
 public class Constants {
-    public static final String SellerOfficeURL = "https://qaso.n11.com/selleroffice/login";
-    public static final String BackOfficeURL = "https://qabo.n11.com/backoffice/main.xhtml";
     public static final String YSWEBURL = "https://www.yemeksepeti.com/osmaniye";
 }
